248ecce52ef1216eb6f9e6e9c1cde5e64185c115
aaqibhafeezkhan-mphasis <aaqibhafeez.khan@mphasis.com>
2026-02-27 06:51:26 +0530

    API errors corrected

---
+++ src/main/java/com/company/monitor/service_health_monitor/scheduler/HealthCheckScheduler.java
@@ -0,0 +1,20 @@
+package com.company.monitor.service_health_monitor.scheduler;
+
+import com.company.monitor.service_health_monitor.service.HealthCheckService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthCheckScheduler {
+
+    private final HealthCheckService healthCheckService;
+
+    public HealthCheckScheduler(HealthCheckService healthCheckService) {
+        this.healthCheckService = healthCheckService;
+    }
+
+    @Scheduled(fixedRateString = "${monitor.scheduler-rate}")
+    public void runHealthCheck() {
+        healthCheckService.checkAllServices();
+    }
+}
--- src/test/java/com/company/monitor/service_health_monitor/ServiceHealthMonitorApplicationTests.java
+++ src/test/java/com/company/monitor/service_health_monitor/ServiceHealthMonitorApplicationTests.java
@@ -3,7 +3,7 @@ package com.company.monitor.service_health_monitor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ServiceHealthMonitorApplication.class)
 class ServiceHealthMonitorApplicationTests {
 
 	@Test
