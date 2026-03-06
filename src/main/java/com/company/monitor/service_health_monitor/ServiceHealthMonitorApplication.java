3003d728a350b50143676331b9aa621a8890437c
aaqibhafeezkhan-mphasis <aaqibhafeez.khan@mphasis.com>
2026-03-06 15:40:05 +0530

    Charts display for the historical API monitoring data

--- src/main/java/com/company/monitor/service_health_monitor/ServiceHealthMonitorApplication.java
+++ src/main/java/com/company/monitor/service_health_monitor/ServiceHealthMonitorApplication.java
@@ -2,9 +2,11 @@ package com.company.monitor.service_health_monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class ServiceHealthMonitorApplication {
 
 	public static void main(String[] args) {
